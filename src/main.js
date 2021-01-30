e765e4d3fd9dbeab0a4236fa8243944a5ff8ec31
Mauricio Covarrubias <mauricioecovarrubias@hotmail.com>
2021-01-29 22:46:15 -0600

    Update main file

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import {rutas} from './rutas.js'
 
 Vue.config.productionTip = false
 
+Vue.use(VueRouter)
+
+const enrutador = new VueRouter({
+  routes:rutas,
+  mode: 'history'
+})
+
 new Vue({
   render: h => h(App),
+  router:enrutador
 }).$mount('#app')
