4523a68d3df99637f7459c10e5a61cb35479fb1c
Mauricio Covarrubias <mauricioecovarrubias@hotmail.com>
2021-01-30 19:48:35 -0600

    Add users routes

--- src/rutas.js
+++ src/rutas.js
@@ -1,4 +1,19 @@
 import InicioComponent from './components/inicio.vue'
+//import UsuariosComponent from './components/usuarios.vue'
+import InfoUsuario from './components/infoUsuario.vue'
+
+const UsuariosComponent = resolve =>{
+    require.ensure(['./components/usuarios.vue'], ()=>{
+        resolve(require('./components/usuarios.vue'))
+    })
+}
+
 export const rutas =[
-    {path:'', component: InicioComponent}
+    {path:'', component: InicioComponent},
+    {path:'/usuarios', component: UsuariosComponent, name: 'usuarios'/*,children: [
+        {path: ':id', component: InfoUsuario, name :'infoUsuario'}
+    ]*/},
+    {path: '/usuarios/:id', component: InfoUsuario, name :'infoUsuario'},
+    {path: '/users', redirect: '/usuarios'},
+    {path: '*', redirect: '/'}
 ]
