6b303d3445ac1fd4a2f069231241413757ed94e9
Mauricio Covarrubias <mauricioecovarrubias@hotmail.com>
2021-01-30 19:49:59 -0600

    Update buttons

--- src/components/encabezado.vue
+++ src/components/encabezado.vue
@@ -2,8 +2,8 @@
     <div class="header clearfix">
         <nav>
             <ul class="nav nav-pills pull-right">
-                <li><a href="">Inicio</a></li>
-                <li><a href="">Uduarios</a></li>
+                <router-link tag="li" to="/" active-class="active" exact><a>Inicio</a></router-link>
+                <router-link tag="li" to="/usuarios/" active-class="active" exact><a>Usuarios</a></router-link>
             </ul>
             <h3 class="text muted">Aplicacion Usuarios</h3>
         </nav>
