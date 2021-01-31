36db2149955b664efa25013601a17ce1da8e5c75
Mauricio Covarrubias <mauricioecovarrubias@hotmail.com>
2021-01-30 19:49:21 -0600

    import and use vue-resource

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
 import {rutas} from './rutas.js'
+
 
 Vue.config.productionTip = false
 
 Vue.use(VueRouter)
+Vue.use(VueResource)
 
 const enrutador = new VueRouter({
   routes:rutas,
